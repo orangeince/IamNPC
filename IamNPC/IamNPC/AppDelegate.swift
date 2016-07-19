9ed95ac836940df499b7de36800d88d0aada6544
赵少龙 <orangeince@outlook.com>
2016-07-19 23:01:31 +0800

    feat: 添加realm swiftyjson pod引用 refactor: 更改文件的组织结构

--- IamNPC/IamNPC/AppDelegate.swift
+++ IamNPC/IamNPC/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let config = Realm.Configuration(
+            schemaVersion: 1,
+            migrationBlock: {
+                migration, oldSchemaVersion in
+            }
+        )
+        Realm.Configuration.defaultConfiguration = config
         return true
     }
 
