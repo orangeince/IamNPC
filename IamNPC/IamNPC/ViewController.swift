50ba6c9887456fd3aba68e163515a0d72372af2a
赵少龙 <orangeince@outlook.com>
2016-07-19 23:22:08 +0800

    追加提交pod文件。。

--- IamNPC/IamNPC/ViewController.swift
+++ IamNPC/IamNPC/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +19,21 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func greetBtnTapped(sender: UIButton) {
+        greetingLabel.hidden = false
+        greetingLabel.alpha = 0.0
+        let origin = greetingLabel.center
+        greetingLabel.center = CGPoint(x: greetingLabel.center.x, y: 0)
+        UIView.animateWithDuration(
+            1.0,
+            animations: {
+                self.greetingLabel.alpha = 1.0
+                self.greetingLabel.center = origin
+            }) { (finished) in
+                
+        }
     }
 
 
